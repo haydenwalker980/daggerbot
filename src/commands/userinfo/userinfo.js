cb883d79460e3a980a62666957af8cf131662755
Hayden <37120379+haydenwalker980@users.noreply.github.com>
2020-09-30 05:23:24 -0700

    Update userinfo.js

--- src/commands/userinfo/userinfo.js
+++ src/commands/userinfo/userinfo.js
@@ -2,6 +2,6 @@ module.exports = {
  name: "userinfo",
  aliases: ["whoami"],
  run: async (client, message, args) => {
-  message.channel.send(`your info:\nuser id: ${message.author.id}\nyour username and tag: ${message.author.tag\nyour avatar (URL): ${message.author.avatarURL}`)
+  message.channel.send(`your info:\nuser id: ${msg.author.id}\nyour username and tag: ${msg.author.tag}\nyour avatar (URL): ${msg.author.displayAvatarURL}`)
  }
 }
